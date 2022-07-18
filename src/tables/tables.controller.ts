0a7a18503ce08e1e53991f667dff5847c6d32dc2
Maycon Morais <maycomorais@gmail.com>
2022-07-18 12:44:34 -0300

    fix tables controler

--- src/tables/tables.controller.ts
+++ src/tables/tables.controller.ts
@@ -6,37 +6,58 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { TablesService } from './tables.service';
 import { CreateTableDto } from './dto/create-table.dto';
 import { UpdateTableDto } from './dto/update-table.dto';
 import { Table } from './entities/table.entity';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
 
+@UseGuards(AuthGuard())
+@ApiTags('tables')
+@ApiBearerAuth()
 @Controller('tables')
 export class TablesController {
   constructor(private readonly tablesService: TablesService) {}
 
   @Post()
-  create(@Body() dto: CreateTableDto): Promise<Table | void> {
+  @ApiOperation({
+    summary: 'Cria uma nova mesa',
+  })
+  create(@Body() dto: CreateTableDto): Promise<Table> {
     return this.tablesService.create(dto);
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Lista todas as mesas',
+  })
   findAll(): Promise<Table[]> {
     return this.tablesService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  @ApiOperation({
+    summary: 'Lista uma mesa',
+  })
+  findOne(@Param('id') id: string): Promise<Table> {
     return this.tablesService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() dto: UpdateTableDto) {
+  @ApiOperation({
+    summary: 'Atualiza uma mesa',
+  })
+  update(@Param('id') id: string, @Body() dto: UpdateTableDto): Promise<Table> {
     return this.tablesService.update(id, dto);
   }
 
   @Delete(':id')
+  @ApiOperation({
+    summary: 'Exclui uma mesa',
+  })
   remove(@Param('id') id: string) {
     return this.tablesService.remove(id);
   }
